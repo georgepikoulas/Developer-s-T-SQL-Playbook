cfc095b01048be1d9f09b68500d22a22181fd745
geopik <geopik@gmail.com>
2023-03-23 10:36:34 +0200

    added DATEDIFFF and order by

--- 09/SlicingDatesWithDatepartReorts.sql
+++ 09/SlicingDatesWithDatepartReorts.sql
@@ -2,6 +2,7 @@ select  InvoiceId, InvoiceDate, Total,
 DATEPART(quarter, InvoiceDate) as quarter,
 DATEPART(month	, InvoiceDate) as month,
 DATEPART(year , InvoiceDate) as year,
-DATEPART(day,InvoiceDate) as day
-
+DATEPART(day,InvoiceDate) as day,
+DATEDIFF([month], '01/01/2005', InvoiceDate) as MonthsInbusiness
 from Invoice
+order by MonthsInbusiness
